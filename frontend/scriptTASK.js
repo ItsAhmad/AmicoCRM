83aeac8a7aed9d3e619e933c48d48bfe45473270
ItsAhmad <ahmad.ameen526@hotmail.com>
2025-01-28 17:04:41 -0500

    working on task UI

--- frontend/scriptTASK.js
+++ frontend/scriptTASK.js
@@ -1,9 +1,18 @@
-const taskForm = document.getElementById('task-form');
+        const taskForm = document.getElementById('task-form');
         const taskInput = document.getElementById('task-input');
         const taskList = document.getElementById('task-list');
         const addTaskBtn = document.getElementById('add-task-btn');
 
+        function deleteTask(taskItem) {
+            if (confirm("Do you want to delete this task?")) {
+                taskItem.remove();
+            }
+        }
+        
         addTaskBtn.addEventListener('click', () => {
+
+            taskInput.style.display = "block";
+
             const taskName = taskInput.value.trim();
 
             if (taskName === '') return;
@@ -18,6 +27,11 @@ const taskForm = document.getElementById('task-form');
             taskNameElement.className = 'task-name';
             taskNameElement.textContent = taskName;
 
+            const taskDeleteButton = document.createElement('div'); 
+            taskDeleteButton.className = 'class-delete-button'
+            taskDeleteButton.attributeStyleMap = '<i class="fa fa-trash-o style="font-size:24px></i>'
+
+
             checkbox.addEventListener('click', () => {
                 checkbox.classList.toggle('completed');
                 taskNameElement.classList.toggle('completed');
@@ -26,6 +40,7 @@ const taskForm = document.getElementById('task-form');
             taskItem.appendChild(checkbox);
             taskItem.appendChild(taskNameElement);
             taskList.appendChild(taskItem);
+            taskItem.appendChild(taskDeleteButton);
 
             taskInput.value = '';
         });
